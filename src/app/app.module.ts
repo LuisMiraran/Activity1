d649eb5ae1dbcd331b755664f8dbeffc8fd77029
LuisMiraran <luisaldrin.miraran@letran.edu.ph>
2024-01-20 17:13:46 +0800

    try

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    ProfileComponent,
+    ContactComponent,
+    EducationComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/education/education.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  styleUrl: '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+})
+export class EducationComponent {
+
+}
